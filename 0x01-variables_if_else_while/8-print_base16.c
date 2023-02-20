0a2b42e951e9044a601a1f755e77f2a55af3108c
Bentsa97 <vbshipalana@gmail.com>
2023-02-20 09:37:31 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,10 +12,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (la = 'a'; la <= 'f'; la++)
-		putchar(la);
-
 	putchar('\n');
 
-	return (0)
+	return (0);
 }
